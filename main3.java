2d419137f3e2720d6d8112cac53f63ac4651dfeb
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-14 19:35:57 +0530

    Add files via upload

---
+++ main3.java
@@ -0,0 +1,12 @@
+public class main3 {
+    public static void main(String[]args){
+        int a=30;
+        int b=20;
+        int diff=a-b;
+        if(diff%2==0)
+            System.out.println("Difference is Even");
+        else
+            System.out.println("Difference is odd");
+    }
+    
+}
