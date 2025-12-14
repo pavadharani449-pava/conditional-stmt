593d46831a20013a642277a8032d7e343fdddb4b
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-14 19:20:55 +0530

    Add files via upload

---
+++ cd1.java
@@ -0,0 +1,13 @@
+package conditional;
+import java.util.Scanner;
+public class cd1 {
+    public static void main(String[]args){
+        int a=10;
+        int b=20;
+        if(a<b)
+            System.out.println(a+" is smaller");
+        else
+            System.out.println(b+"is smaller");
+    }
+    
+}
