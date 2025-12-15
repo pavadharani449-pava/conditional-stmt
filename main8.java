43ec245d00f38461cc319881f29da852b6ba30b7
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-15 08:48:36 +0530

    Add files via upload

---
+++ main8.java
@@ -0,0 +1,14 @@
+public class main8 {
+    public static void main(String[]args){
+        int a=12;
+        int b=22;
+        int c=16;
+        if(a>=b&&a>=c)
+            System.out.println(a+"is the greater");
+        else if(b>=a && b>=c)
+            System.out.println(b+"is the greater");
+        else
+            System.out.println(c+"is the greater");
+    }
+    
+}
