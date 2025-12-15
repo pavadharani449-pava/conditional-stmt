8d65fee2138af5d46d83ccba3ec84aed2582521d
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-15 08:05:01 +0530

    Add files via upload

---
+++ main6.java
@@ -0,0 +1,10 @@
+public class main6 {
+    public static void main(String[]args){
+        int n=345;
+        if(n>=100&&n<=999)
+            System.out.println("It is a 3-digit number");
+        else
+            System.out.println("It is not a 3-digit number");
+
+    }
+}
