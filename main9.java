cc1cac865472f436f5c95409e14e52f3ca466861
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-15 08:53:03 +0530

    Add files via upload

---
+++ main9.java
@@ -0,0 +1,11 @@
+public class main9 {
+    public static void main(String[]args){
+        int a=12;
+        int b=22;
+        int c=16;
+        if(a<=b&&a<=c)
+            System.out.println(a+"is the smaller");
+        else if(b<=a && b<=c)
+            System.out.println(c+"is the smaller");
+    }
+}
