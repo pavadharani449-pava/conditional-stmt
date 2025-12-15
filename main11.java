35e56ebe6f146a32cc2a4e8650510626ca613f35
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-15 09:02:18 +0530

    Add files via upload

---
+++ main11.java
@@ -0,0 +1,9 @@
+public class main11 {
+    public static void main(String[]args){
+        int n=3;
+        if((n&1)==0)
+            System.out.println("Even number");
+        else
+            System.out.println("odd number");
+    }
+}
