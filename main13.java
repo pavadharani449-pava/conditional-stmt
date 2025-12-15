f56613b50dc51e4251a175d985312d7cea8d278b
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-15 09:12:42 +0530

    Add files via upload

---
+++ main13.java
@@ -0,0 +1,10 @@
+public class main13 {
+    public static void main(String []args){
+        int n=345;
+        int firstDigit=n%100;
+        if(firstDigit%2==0)
+            System.out.println("First digit is Even");
+        else
+            System.out.println("First digit is Odd");
+    }
+}
