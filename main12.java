9f48ab6769704787d94bf0590c5ea5e2071e83c8
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-15 09:05:55 +0530

    Add files via upload

---
+++ main12.java
@@ -0,0 +1,11 @@
+public class main12 {
+    public static void main(String[]args){
+        int a=8;
+        int b=9;
+        int sum=a+b;
+        if(sum%2==0)
+            System.out.println("sum is even");
+        else
+            System.out.println("sum is odd");
+    }
+}
