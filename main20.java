f27f9f8215ef653d6b62b5b1274956a1f3246ded
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-15 09:54:49 +0530

    Add files via upload

---
+++ main20.java
@@ -0,0 +1,9 @@
+public class main20 {
+    public static void main(String[]args){
+        int n=30;
+        if(n%3==0 && n%5==0)
+            System.out.println("Number is divisible by both 3 abd 5");
+        else
+            System.out.println("Number is not divisible by both 3 abd 5");
+    }
+}
