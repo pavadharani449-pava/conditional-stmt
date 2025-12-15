f86a3d2654ea901614a1212c82f40a6bb12a8b35
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-15 09:18:09 +0530

    Add files via upload

---
+++ main14.java
@@ -0,0 +1,10 @@
+public class main14 {
+    public static void main(String[]args){
+        int n=246;
+        int lastDigit=n%10;
+        if(lastDigit % 2==0)
+            System.out.println("Last Digit is Even");
+        else
+            System.out.println("Last Digit is odd");
+    }
+}
