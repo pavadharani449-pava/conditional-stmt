4bec7b9739056b050c7d95666cfbca1f330530c3
pavadharani449-pava <pavadharani449@gmail.com>
2025-12-15 09:35:17 +0530

    Add files via upload

---
+++ main17.java
@@ -0,0 +1,9 @@
+public class main17 {
+    public static void main(String []args){
+        int n=21;
+        if(n%3==0 && n% 7==0)
+            System.out.println("Number is divisible by 3 or 7");
+        else
+            System.out.println("Number is not divisible by both 3 and 5");c
+    }
+}
